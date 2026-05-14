55985982795b6ff56c430c2339bbb967b308feb9
Noam <noamroger120606@gmail.com>
2026-05-14 20:34:14 +0200

    change github link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://noamroger.fr',
+	site: 'https://www.noamroger.fr',
 	base: '/',
 });
